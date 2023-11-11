b2d02eb1a06ada88da871a545d9e2f348ea5ff83
nwjbrandon <nwjbrandon@gmail.com>
2023-11-12 00:18:27 +0800

    update

--- train.py
+++ train.py
@@ -1,15 +1,14 @@
 import os
 import pathlib
 
-from stable_baselines.common.callbacks import CallbackList, CheckpointCallback
+from stable_baselines.common.callbacks import CallbackList, CheckpointCallback, EvalCallback
 
-from callbacks import PlotCallback
 from config import model_config
 from gym_warehouse.envs import WarehouseEnv
 
 os.environ["KMP_WARNINGS"] = "0"
 
-model_name = "dqn"
+model_name = "ppo2"
 save_freq = 50000
 ckpt_fpath = f"./models/{model_name}"
 pathlib.Path(ckpt_fpath).mkdir(exist_ok=True)
@@ -19,27 +18,27 @@ RLModel = model_config[model_name]["model"]
 total_timesteps = model_config[model_name]["total_timesteps"]
 config = model_config[model_name]["config"]
 
-# Create callbacks
-plot_callback = PlotCallback(
-    episode_plot_freq=total_timesteps,
-    update_stats_every=1,
-    average_size=100,
-    verbose=1,
-    plot_prefix=model_name,
-    plot_dir=ckpt_fpath,
-)
-checkpoint_callback = CheckpointCallback(
-    save_freq=save_freq, save_path=ckpt_fpath, name_prefix=model_name
-)
-callbacks = CallbackList([checkpoint_callback, plot_callback])
-
 # Create model and environment
 env = WarehouseEnv("6x5_4bins_1item_1slot")
 model = RLModel("MlpPolicy", env, verbose=1, **config)
 
+# Create callbacks
+checkpoint_callback = CheckpointCallback(
+    save_freq=save_freq, save_path=ckpt_fpath, name_prefix=model_name
+)
+eval_callback = EvalCallback(
+    env,
+    best_model_save_path=ckpt_fpath,
+    log_path=ckpt_fpath,
+    eval_freq=50000,
+    deterministic=True,
+    render=False,
+)
+callbacks = CallbackList([checkpoint_callback, eval_callback])
+
 # Train Model
 model.learn(total_timesteps=total_timesteps, callback=callbacks)
-model.save(f"{ckpt_fpath}/best")
+model.save(f"{ckpt_fpath}/last")
 
 
 obs = env.reset()
