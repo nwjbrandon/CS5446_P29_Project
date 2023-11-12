c1cd797c86e908f33540754baef5eabb60b1857c
Brandon Ng <nwjbrandon@gmail.com>
2023-11-12 19:45:58 +0800

    feat: update results for 6x5_4bins_1item_1slot environment (#2)

--- train.py
+++ train.py
@@ -1,37 +1,53 @@
 import os
 import pathlib
 
-from stable_baselines.common.callbacks import CallbackList, CheckpointCallback
+import numpy as np
+from stable_baselines.common.callbacks import CallbackList, CheckpointCallback, EvalCallback
 
 from config import model_config
 from gym_warehouse.envs import WarehouseEnv
 
 os.environ["KMP_WARNINGS"] = "0"
 
-model_name = "a2c"
-save_freq = 50000
-ckpt_fpath = f"./models/{model_name}"
-pathlib.Path(ckpt_fpath).mkdir(exist_ok=True)
+np.random.seed(42)
 
-# Callbacks
-checkpoint_callback = CheckpointCallback(
-    save_freq=save_freq, save_path=ckpt_fpath, name_prefix=model_name
-)
-callbacks = CallbackList([checkpoint_callback])
+env_name = "6x5_4bins_1item_1slot"
+model_name = "dqn"
+save_freq = 5000
+eval_freq = 5000
+n_eval_episodes = 30
+ckpt_fpath = f"./models/{env_name}/{model_name}"
+pathlib.Path(ckpt_fpath).mkdir(exist_ok=True, parents=True)
 
-# Create model and environment
+# Get config
 RLModel = model_config[model_name]["model"]
 total_timesteps = model_config[model_name]["total_timesteps"]
 config = model_config[model_name]["config"]
 
-env = WarehouseEnv("6x5_4bins_1item_1slot")
+# Create model and environment
+env = WarehouseEnv(env_name)
 model = RLModel("MlpPolicy", env, verbose=1, **config)
+
+# Create callbacks
+checkpoint_callback = CheckpointCallback(
+    save_freq=save_freq, save_path=ckpt_fpath, name_prefix=model_name
+)
+eval_callback = EvalCallback(
+    env,
+    best_model_save_path=ckpt_fpath,
+    log_path=ckpt_fpath,
+    n_eval_episodes=n_eval_episodes,
+    eval_freq=eval_freq,
+    deterministic=True,
+    render=False,
+)
+callbacks = CallbackList([checkpoint_callback, eval_callback])
 
 # Train Model
 model.learn(total_timesteps=total_timesteps, callback=callbacks)
-model.save(f"{ckpt_fpath}/best")
+model.save(f"{ckpt_fpath}/last_model")
 
-
+# Evaluate Model
 obs = env.reset()
 n_steps = 300
 total = 0
